b66c6c5bc8732ead6367ee125055e5e0f07cdb06
hyperorchid <hyperorchid@outlook.com>
2020-05-24 12:44:43 +0800

    save

--- ios/bmailI.go
+++ ios/bmailI.go
@@ -7,6 +7,7 @@ import (
 	"github.com/BASChain/go-bmail-protocol/bmp"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/google/uuid"
+	"time"
 )
 
 type EnvelopeOfUI struct {
@@ -14,29 +15,24 @@ type EnvelopeOfUI struct {
 	Subject  string   `json:"sub"`
 	MsgBody  string   `json:"msg"`
 	From     string   `json:"from"`
+	FromName string   `json:"fromName"`
 	TOs      []string `json:"tos"`
 	CCs      []string `json:"ccs"`
 	BCCs     []string `json:"bccs"`
+	Date     string   `json:"date"`
 	MailType int8     `json:"mType"`
 }
 
 var bmClient *bmp.BMailClient = nil
 
 type MailSendCallBack interface {
-	Success(iv []byte, enSub, enMsg string)
-	Error(typ int, msg string)
+	MailSendProcess(typ int, msg string)
 }
 
-func NewMailClient() bool {
+func newClient() (*bmp.BMailClient, error) {
 
 	if basResolver == nil {
-		fmt.Println("no valid bas resolver")
-		return false
-	}
-
-	if activeWallet == nil {
-		fmt.Println("no valid wallet")
-		return false
+		return nil, fmt.Errorf("no valid bas resolver")
 	}
 
 	conf := &bmp.ClientConf{
@@ -46,37 +42,51 @@ func NewMailClient() bool {
 
 	bc, err := bmp.NewClient(conf)
 	if err != nil {
-		fmt.Println(err)
-		return false
+		return nil, err
 	}
 
-	bmClient = bc
-	return true
+	return bc, nil
+}
+
+func CloseClient() {
+	if bmClient != nil {
+		bmClient.Close()
+		bmClient = nil
+	}
 }
 
 func SendMailJson(mailJson string, cb MailSendCallBack) bool {
 
+	if activeWallet == nil || !activeWallet.IsOpen() {
+		fmt.Println("wallet is nil or locked")
+		cb.MailSendProcess(BMErrWalletInvalid, "wallet is nil or locked")
+		return false
+	}
+
+	if bmClient == nil {
+		bc, err := newClient()
+		if err != nil {
+			fmt.Println(err.Error())
+			cb.MailSendProcess(BMErrClientInvalid, err.Error())
+			return false
+		}
+		bc.Wallet = activeWallet
+		bmClient = bc
+	}
+
 	fmt.Println(mailJson)
 	jsonMail := &EnvelopeOfUI{}
 	if err := json.Unmarshal([]byte(mailJson), jsonMail); err != nil {
-		uiCallback.Error(BMErrInvalidJson, "mail json data is invalid")
+		fmt.Println("mail json data is invalid", err)
+		uiCallback.Error(BMErrInvalidJson, err.Error())
 		return false
 	}
 	fmt.Println(jsonMail)
 
-	if bmClient == nil {
-		cb.Error(BMErrClientInvalid, "mail client is invalid")
-		return false
-	}
-
-	if activeWallet == nil || !activeWallet.IsOpen() {
-		cb.Error(BMErrWalletInvalid, "wallet is nil or locked")
-		return false
-	}
-
 	toAddr, _ := basResolver.BMailBCA(jsonMail.TOs[0])
 	if !toAddr.IsValid() {
-		cb.Error(BMErrNoSuchBas, "can't find receiver's block chain info")
+		fmt.Println("can't find receiver's block chain info")
+		cb.MailSendProcess(BMErrNoSuchBas, "can't find receiver's block chain info")
 		return false
 	}
 
@@ -86,6 +96,8 @@ func SendMailJson(mailJson string, cb MailSendCallBack) bool {
 			From:     activeWallet.MailAddress(),
 			FromAddr: activeWallet.Address(),
 			To:       jsonMail.TOs[0],
+			ToAddr:   toAddr,
+			Date:     time.Now(),
 		},
 		EnvelopeBody: bmp.EnvelopeBody{
 			Subject: jsonMail.Subject,
@@ -93,11 +105,20 @@ func SendMailJson(mailJson string, cb MailSendCallBack) bool {
 		},
 	}
 
-	if err := bmClient.SendP2pMail(env); err != nil {
-		cb.Error(BMErrSendFailed, err.Error())
-		return false
+	if jsonMail.MailType == bmp.BMailModeP2P {
+		if err := bmClient.SendP2pMail(env); err != nil {
+			fmt.Println(err.Error())
+			cb.MailSendProcess(BMErrSendFailed, err.Error())
+			return false
+		}
+	} else {
+		if err := bmClient.SendP2sMail(env); err != nil {
+			fmt.Println(err.Error())
+			cb.MailSendProcess(BMErrSendFailed, err.Error())
+			return false
+		}
 	}
-
+	fmt.Println("success------->")
 	return true
 }
 
@@ -137,18 +158,18 @@ func Decode(data string) string {
 func SendCryptMail(eid, to, sub, msg string, cb MailSendCallBack) bool {
 
 	if bmClient == nil {
-		cb.Error(BMErrClientInvalid, "mail client is invalid")
+		cb.MailSendProcess(BMErrClientInvalid, "mail client is invalid")
 		return false
 	}
 
 	if activeWallet == nil || !activeWallet.IsOpen() {
-		cb.Error(BMErrWalletInvalid, "wallet is nil or locked")
+		cb.MailSendProcess(BMErrWalletInvalid, "wallet is nil or locked")
 		return false
 	}
 
 	toAddr, _ := basResolver.BMailBCA(to)
 	if !toAddr.IsValid() {
-		cb.Error(BMErrNoSuchBas, "can't find receiver's block chain info")
+		cb.MailSendProcess(BMErrNoSuchBas, "can't find receiver's block chain info")
 		return false
 	}
 
@@ -166,9 +187,9 @@ func SendCryptMail(eid, to, sub, msg string, cb MailSendCallBack) bool {
 	}
 
 	if err := bmClient.SendP2pMail(env); err != nil {
-		cb.Error(BMErrSendFailed, err.Error())
+		cb.MailSendProcess(BMErrSendFailed, err.Error())
 		return false
 	}
-
+	cb.MailSendProcess(BMErrNone, "Success")
 	return true
 }
